6b96d912d746987da9fd8f51f9dad8711d512931
Adam Pah <adamrpah@gmail.com>
2015-04-20 11:22:51 -0500

    standard commit

--- conf.py
+++ conf.py
@@ -388,11 +388,15 @@ REDIRECTIONS = []
 # to `nikola deploy`.  If no arguments are specified, a preset
 # named `default` will be executed.  You can use as many presets
 # in a `nikola deploy` command as you like.
-# DEPLOY_COMMANDS = {
-#     'default': [
-#         "rsync -rav --delete output/ joe@my.site:/srv/www/site",
-#     ]
-# }
+DEPLOY_COMMANDS = {
+    'default': [
+        "cd output",
+        "nikola build",
+        "git add --all",
+        "git commit -m 'standard commit'",
+        "git push origin master",
+    ]
+}
 
 # For user.github.io OR organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
