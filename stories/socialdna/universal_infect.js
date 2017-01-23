b1723c44d5008385dc6a57d3a552f8e4242848ff
Adam Pah <adamrpah@gmail.com>
2017-01-23 10:40:00 -0600

    updated game thanks to abe!

--- stories/socialdna/universal_infect.js
+++ stories/socialdna/universal_infect.js
@@ -1,12 +1,31 @@
 $("#restart").click(function(){
-  viz_change()
+
+  viz_change("scale_free.json")
 })
 
+
+$("#home_page").click(function(){
+  $('#chart1').show();
+  $('#chart2').show();
+  $('#second_row').show();
+  $('#node_cost').show();
+  $('#coins_remaining').show();
+  $('#high_scores_table').hide();
+  $('#high_scores').html('High Scores');
+})
+
+var names = [];
+var toggle = 0;
+var toggle_2 = 0;
 var main = function(netfile){
+  $('#high_scores_table').hide();
+  $('#saver_form').hide();
+  var p1Coins = 50;
+  var p2Coins = 50;
   d3.json(netfile, function(error, graph) {
     var player = 'Player 1';
-    var p1Coins = 50;
-    var p2Coins = 50;
+    // var p1Coins = 50;
+    // var p2Coins = 50;
     function findNeighbors(node) {
         var neighbors = [];
         for (var i = 0; i < graph.links.length; i++) {
@@ -16,18 +35,29 @@ var main = function(netfile){
         }
         return neighbors;
     }
-
-    function findLinks(node) {
-      var neighborLinks = [];
-      for (var i = 0; i < graph.links.length; i++) {
-          var testLink = graph.links[i];
-          if (testLink.source == node)
-           neighborLinks.push(graph.links[i]);
-
-           //find source, and find target and redraw line??
-      }
-      return neighborLinks;
+    function findNeighborLinks(node) {
+        var neighborLinks = [];
+        for (var i = 0; i < graph.links.length; i++) {
+            var testLink = graph.links[i];
+            if (testLink.source == node)
+            neighborLinks.push(i);
+            if (testLink.target == node)
+            neighborLinks.push(i);
+        }
+        return neighborLinks;
     }
+    //maybe
+    // function findL sinks(node) {
+    //   var neighborLinks = [];
+    //   for (var i = 0; i < graph.links.length; i++) {
+    //       var testLink = graph.links[i];
+    //       if (testLink.source == node)
+    //        neighborLinks.push(graph.links[i]);
+    //
+    //        //find source, and find target and redraw line??
+    //   }
+    //   return neighborLinks;
+    // }
 
     function findNeighborColors(node) {
         var neighbors = [];
@@ -68,7 +98,7 @@ var main = function(netfile){
         return {'black': nblack, 'green': ngreen, 'blue': nblue};
     }
 
-    function nodeClick(d, i) {   ///WHERE Do the arguments come from? They're not referenced where nodeClick is called
+    function nodeClick(d, i) {
       var green = 'rgb(0, 128, 0)';
       var blue = 'rgb(0, 0, 255)';
       var black = 'rgb(0, 0, 0)';
@@ -83,7 +113,7 @@ var main = function(netfile){
         var sugg_fill_color = 'rgb(0, 128, 0)';
         var sugg_fill_color_label = 'green';
         p1Coins = p1Coins - nodeCost;
-        coinText.text("Coins Remaining: " + p1Coins);
+        document.getElementById("coins_remaining").innerHTML = "Coins Remaining: " + p1Coins;
         inode.style('fill', sugg_fill_color);
         nodes[i]['color'] = sugg_fill_color_label;
         }
@@ -91,13 +121,13 @@ var main = function(netfile){
           inode.style('fill', 'black');
           nodes[i]['color'] = black;
           p1Coins = parseInt(p1Coins) + parseInt(nodeCost);
-          coinText.text("Coins Remaining: " + p1Coins);
+          document.getElementById("coins_remaining").innerHTML = "Coins Remaining: " + p1Coins;
         }
         else if (node_color == 'rgb(0, 0, 255)') {
-          window.alert("Uh, this isn't your node.");
+          window.alert("Uh, this isn't your node, dude.");
         }
         else {
-          window.alert("You Don't have enough bears!");
+          window.alert("You don't have enough coins!");
         }
 
       }
@@ -108,21 +138,22 @@ var main = function(netfile){
         var sugg_fill_color = 'rgb(0, 0, 255)';
         var sugg_fill_color_label = 'blue';
         p2Coins = p2Coins - nodeCost;
-        coinText.text("Coins Remaining: " + p2Coins);
+        // coinText.text("Coins Remaining: " + p2Coins);
         inode.style('fill', sugg_fill_color);
         nodes[i]['color'] = sugg_fill_color_label;
+        document.getElementById("coins_remaining").innerHTML = "Coins Remaining: " + p2Coins;
         }
         else if (node_color == 'rgb(0, 0, 255)') {
           inode.style('fill', 'rgb(0, 0, 0)');
           nodes[i]['color'] = black;
           p2Coins = parseInt(p2Coins) + parseInt(nodeCost);
-          coinText.text("Coins Remaining: " + p2Coins);
+          document.getElementById("coins_remaining").innerHTML = "Coins Remaining: " + p2Coins;
         }
         else if (node_color == 'rgb(0, 128, 0)') {
-          window.alert("Uh, this isn't your node.");
+          window.alert("Uh, this isn't your node, dude.");
         }
         else {
-          window.alert("You Don't have enough coins!");
+          window.alert("You don't have enough coins!");
         }
       }
 
@@ -132,43 +163,43 @@ var main = function(netfile){
 
 //Hover functions
     function hoverFunc (d, i) {
+      //get node of interest
       var inode = d3.select(this);
       var hoverRadius = inode.attr("r");
+      var hoverDegree = findNeighbors(nodes[i]).length;
+      //color node of interest
+      inode.style("stroke", "grey").style("stroke-width", "5px");
+      infoText.text("Node Cost: " + hoverDegree);
+      document.getElementById("node_cost").innerHTML = "Node Cost: " + hoverDegree;
+      //for the nearby neighbornodes
       var neighborNodes = findNeighbors(nodes[i]);
-      var hoverDegree = neighborNodes.length;
-      var neighborLinks = findLinks(nodes[i]);
-      // var link = svg.selectAll(".link")
-      //     .data(graph.links)
-      //   .enter().append("line")
-      //     .attr("class", "link")
-      //     .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-      // link.style("stroke-width", "3px");
-      // neighborLinks.style("stroke-width","10px");
-      // window.alert(graph.links[1]);
-
       for (ni in neighborNodes) {
         //neighbor node colors
         d3.select(node[0][neighborNodes[ni].index]).style("stroke", "rgb(250, 160, 122)").style("stroke-width", "3px");
       }
-      infoText.text("Node Cost: " + hoverDegree);
-      //hovered node color
-      inode.style("stroke", "rgb(255, 0, 0)")
-      .style("stroke-width", "5px");
+      var neighborLinks = findNeighborLinks(nodes[i]);
+      for (li in neighborLinks) {
+        d3.select(link[0][neighborLinks[li]]).style("stroke", "rgb(250, 160, 122)").style("stroke-width", "3px");
+      }
 
+      // d3.selectAll(".link").style("stroke-width", "3px" ); //works
+      // d3.select(graph.links).style("stroke-width", "3px" );  //doesn't work
     }
+
     function noHover (d, i) {
       var inode = d3.select(this);
       var neighborNodes = findNeighbors(nodes[i]);
+      var neighborLinks = findNeighborLinks(nodes[i]);
       inode.style("stroke-width", "1.5px").style("stroke", "white");
-      infoText.text("Node Cost: " );
-
+      document.getElementById("node_cost").innerHTML = "Node Cost: ";
 
       for (ni in neighborNodes) {
         d3.select(node[0][neighborNodes[ni].index]).style("stroke", "white").style("stroke-width", "1.5px");
       }
+      for (li in neighborLinks) {
+        d3.select(link[0][neighborLinks[li]]).style("stroke", "grey").style("stroke-width", "1px");
+      }
     }
-
 
 
 
@@ -241,13 +272,12 @@ var main = function(netfile){
            player = 'Player 2';
            $(this).text("Player 2");
            d3.select("#button_labels").attr("value", "Player 2");
-
-           coinText.text("Coins Remaining: " + p2Coins);
+           document.getElementById("coins_remaining").innerHTML = "Coins Remaining: " + p2Coins;
       } else {
           player = 'Player 1';
           $(this).text("Player 1");
           d3.select("#button_labels").attr("value", "Player 1");
-          coinText.text("Coins Remaining: " + p1Coins);
+          document.getElementById("coins_remaining").innerHTML = "Coins Remaining: " + p1Coins;
       }
     })
 
@@ -287,23 +317,22 @@ var main = function(netfile){
 
     // var player = 'Player 1';
     var thresholder = 'Threshold On';
-    var width = 900;
-    var height = 800;
+    var width = 700;
+    var height = 600;
 
     var nodeData = [""];
 
     $("#chart1").width(width + "px");
     $("#chart1").height(height + "px");
-
-    var margin = {top: 20, right: 20, bottom: 30, left: 40};
-    var width = width - 100 - margin.left - margin.right;
-    var height = height - 100 - margin.top - margin.bottom;
+    var margin = {top: 0, right: 0, bottom: 0, left:0}; //{top: 20, right: 20, bottom: 30, left: 40};
+    // var width = width - 100 - margin.left - margin.right;
+    // var height = height - 100 - margin.top - margin.bottom;
 
     var svg = d3.select("#chart1").append("svg")
       .style("position", "relative")
-      .style("max-width", "960px")
+      .style("max-width", "700px")
       .attr("width", width + "px")
-      .attr("height", (height - 50) + "px")
+      .attr("height", height + "px")
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -320,6 +349,7 @@ var main = function(netfile){
         .links(graph.links)
         .start();
 
+//Draws links between nodes
     var link = svg.selectAll(".link")
         .data(graph.links)
       .enter().append("line")
@@ -375,6 +405,7 @@ var main = function(netfile){
     var nodeInfo = d3.select("#infoWindowChart")
        .append("svg:svg")
        .attr("width", width/2)
+       .style("float","left")
        .attr("height", '80px')
        .append("svg:g");
     var infoText = nodeInfo.selectAll("text")
@@ -382,27 +413,28 @@ var main = function(netfile){
         .enter()
         .append("text");
     var textLabels = infoText.attr("x", 30)
-        .attr("y", 40)
+        .attr("y",43)
         .text("Node Cost: ")
         .attr("font-family", "sans-serif")
         .attr("font-size", "14px")
-        .attr("fill", "red");
-
-    var coinCounterChart = d3.select("#infoWindowChart")
-       .append("svg:svg")
-       .attr("width", width/2)
-       .attr("height", '80px')
-       .append("svg:g");
-  var coinText = coinCounterChart.selectAll("text")
-       .data([""])
-       .enter()
-       .append("text");
-    var coinCounterLabels = coinText.attr("x", 30)
-        .attr("y", 50)
-        .text("Coins Remaining: " + p1Coins)
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "14px")
         .attr("fill", "black");
+
+  //   var coinCounterChart = d3.select("#infoWindowChart")
+  //      .append("svg:svg")
+  //     //  .style("float","left")
+  //      .attr("width", width/2)
+  //      .attr("height", '80px')
+  //      .append("svg:g");
+  // var coinText = coinCounterChart.selectAll("text")
+  //      .data([""])
+  //      .enter()
+  //      .append("text");
+  //   var coinCounterLabels = coinText.attr("x", 30)
+  //       .attr("y", 40)
+  //       .text("Coins Remaining: " + p1Coins)
+  //       .attr("font-family", "sans-serif")
+  //       .attr("font-size", "14px")
+  //       .attr("fill", "black");
 
     var svg = d3.select("#chart2").append("svg:svg")
        .attr("width", width)
@@ -418,6 +450,7 @@ var main = function(netfile){
          .attr("height", 20)
          .style('fill', function(d,i) { return keys[i]; });
 
+//This is drawing the ticks for the measure bar
    svg.selectAll("line")
           .data(x.ticks(10))
         .enter().append("svg:line")
@@ -442,25 +475,157 @@ var main = function(netfile){
           .attr("y2", 120)
           .attr("stroke", "#000");
 
-  });
 
-}
+          $("#saver").click(function(){
+            if (toggle_2 ==0){
+            $('#saver_form').show();
+            toggle_2 = 1;
+          }
+            else {
+              $('#saver_form').hide();
+              toggle_2 = 0;
+            }
+          });
 
-var viz_change = function(){
+          $(document).on('click', '#player_myButton', function() {
+            var $player_name = $("input[name=player_form]").val();
+
+            // var namePair = {player_name_2:'', score:2};
+            $('#saver_form').hide();
+            $("#table_entries").remove();
+            if (player=='Player 1') {
+            names.push({playerName:$player_name, nodeCount:countNodeColors('green')});
+          }
+          else {
+            names.push({playerName:$player_name, nodeCount:countNodeColors('blue')});
+          }
+            //here need to work on sorting thesee before moving on to the next part
+            names.sort(function(aa,bb){
+              return aa.nodeCount - bb.nodeCount
+            })
+            var html_table = '';
+            var rank = 1;
+
+            for (var score_i = names.length - 1; score_i >= 0 ;) {
+              html_table = html_table + '<tr id="table_entries"><th scope="row">' + rank +'</th><td>'+names[score_i].playerName+'</td><td>'+names[score_i].nodeCount+'</td></tr>';
+              score_i = score_i - 1;
+              rank = rank + 1;
+            }
+
+            $('#table_entries').remove();
+            $('#highScoreTable').append(html_table);
+
+
+          });
+
+
+          //HERE //here we're trying to dynamically update the table elements from the array names[], who's even elements are names and whose odd are the corresponding scores
+          // $(document).ready(function() {
+            $(document).on('click', '#high_scores', function(){
+              if (toggle == 0) {
+            $('#chart1').hide();
+            $('#chart2').hide();
+            $('#second_row').hide();
+            $('#node_cost').hide();
+            $('#coins_remaining').hide();
+            $('#high_scores_table').show();
+            $('#high_scores').html('Keep Playing');
+
+
+            // var table_open = '<table class="table" id="high_scores_table" style="margin-top:100px;">\
+            //   <thead>\
+            //     <tr>\
+            //       <th>Rank</th>\
+            //       <th>Name</th>\
+            //       <th>Score</th>\
+            //     </tr>\
+            //   </thead>\
+            //   <tbody id="highScoreTable">';
+
+            // $('#row_top').append(table_open + html_table + '</tbody></table>');
+
+
+
+            toggle = 1;
+          }//end of toggle
+          else {
+            $('#chart1').show();
+            $('#chart2').show();
+            $('#second_row').show();
+            $('#node_cost').show();
+            $('#coins_remaining').show();
+            $('#high_scores_table').hide();
+            $('#high_scores').html('High Scores');
+            toggle = 0;
+          }
+            // ($('<tr><th scope="row">' + names[0] +'</th><td>cash</td><td>joe</td></tr>'));
+
+
+
+          });
+
+
+
+
+            // for (var score_i = 0; score_i < names.length; score_i ++) {
+            //   var rank = score_i + 1;
+            //   $('#highScoreTable').append($('<tr><th scope="row">' + rank + '</th><td>bill</td><td>joe</td></tr>'));
+            // }
+          // });
+
+
+
+
+
+  }); //end of d3.json
+
+
+
+
+
+}//end of main
+
+var viz_change = function(netfile){
   //remove the svg
   $('svg').remove();
   //set up the labels
+  $('#coins_remaining').html("Coins Remaining: 50");
   $('#button_labels').text("Player 1");
   d3.select("#button_labels").attr("value", "Player 1");
   //run i tup the flag pole
-  var netfile = $('#data_sources').chosen().val();
   main(netfile)
 }
 
 //Code calls
 ////Set up chosen
-$(".chzn-select").chosen();
+// $(".chzn-select").chosen();
 ////Call the viz_change immediately to initialize
-viz_change();
+viz_change("caveman.json");
 ////Selector box change code.  All use the same function, since combined they identify the file
-$("#data_sources").chosen().change(viz_change)
+// $("#data_sources").chosen().change(viz_change);
+
+//////////////
+$('#scale_f').on('click', function(){
+  viz_change("scale_free.json");
+});
+
+$('#clustered_net').on('click', function(){
+  viz_change("caveman.json");
+});
+
+$(document).ready(function() {
+      $('form#player_form').keyup(function() {
+          var empty = false;
+          $('.player_field input').each(function() {
+              if ($(this).val().length == 0) {
+                  empty = true;
+              }
+          });
+          if (empty) {
+              $('.form-group button').attr('disabled', 'disabled');
+          } else {
+              $('.form-group button').removeAttr('disabled');
+          }
+      });
+
+  });
